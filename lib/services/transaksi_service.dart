c31426809a4603b6c9a5616c95e17927bcdf5b17
abdairsya <121345790+abdairsyaputramirudin@users.noreply.github.com>
2025-07-07 18:03:24 +0700

    Update transaksi_service.dart

--- lib/services/transaksi_service.dart
+++ lib/services/transaksi_service.dart
@@ -1,16 +1,1 @@
-import 'package:hive/hive.dart';
-import '../models/transaksi.dart';
 
-class TransaksiService {
-  final _box = Hive.box<Transaksi>('transaksiBox');
-
-  List<Transaksi> getAll() => _box.values.toList();
-
-  void tambahTransaksi(Transaksi transaksi) {
-    _box.add(transaksi);
-  }
-
-  List<Transaksi> getByBarang(String barangId) {
-    return _box.values.where((e) => e.barangId == barangId).toList();
-  }
-}
